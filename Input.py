cbc877008041104e0712fdc2e571ce3d5fee4a00
PLI Harvey <pli.harvey@gmail.com>
2013-06-11 19:35:15 -0400

    Input.py
    Encountered problem with showing the input on screen as it is typed.
    Taking and using final input works fine.
    
    Would like to see the input being typed though.

--- Input.py
+++ Input.py
@@ -13,17 +13,39 @@ pygame.init()
 class Textbox:
         
     #output the content
-    def label(self, surface, toPrint, colour, size, x, y):
-        font = pygame.font.SysFont("None", size)
-        myText = font.render(toPrint, 1, colour)
-        surface.blit(myText, (x,y))
+    def label(self, toPrint):
+        font = pygame.font.SysFont("None", 10)
+        myText = font.render(toPrint, 1, (0,0,0))
+        return myText
 
     #draw/create
     def create(self, surface, colour, length, height, x, y):
         pygame.draw.rect(surface,(255,255,255), (x,y,length,height),0)
         pygame.draw.rect(surface,(0,0,0),(x,y,length,height),1)
         self.rect = pygame.Rect(x,y,length,height)
-        return surface
+        #return surface
+
+    #Take input
+    def takeInput(self, surface):
+        keepGoing = True
+        text = ""
+        x = self.rect.topleft[0]
+        y = self.rect.topleft[1]
+        while keepGoing:
+            for event in pygame.event.get():
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_RETURN:
+                        keepGoing = False
+                    elif event.key == pygame.K_0:
+                        text += "0"
+            myText = self.label(text)
+            surface.blit(myText, (x,y))
+        return text
+     #problem: cannot see input inside textbox being written
+    #b/c txtbox.blit in main keeps over writting it.
+    # must find way to suspend txtbox.blit, and blit with this instead
+    # perhaps returning a parital text?
+        # would have to run input loop in the main.... ick.
 
     #
     def onClick(self,mouse):
@@ -31,7 +53,6 @@ class Textbox:
             if mouse[1] > self.rect.topleft[1]:
                 if mouse[0] < self.rect.bottomright[0]:
                     if mouse[1] < self.rect.bottomright[1]:
-                        
                         return True
                     else:
                         return False
